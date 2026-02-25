36eeab5eb89c3bfd1db8b67d812c449b4556410e
Adrien Vergnaud <a.vergnaud@gmail.com>
2026-02-25 10:05:39 +0100

    acronyms

--- elearning/urls.py
+++ elearning/urls.py
@@ -1,7 +1,7 @@
 """Root URL configuration for the elearning project."""
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     # Home → redirect to quiz catalogue (login required there)
@@ -12,6 +12,12 @@ urlpatterns = [
     path("quizzes/", include("apps.quizzes.urls", namespace="quizzes")),
     # Admin panel (quiz management + reporting)
     path("admin-panel/", include("apps.quizzes.admin_urls", namespace="admin-panel")),
+    # Static resource pages
+    path(
+        "ressources/glossaire-pci-ssf/",
+        TemplateView.as_view(template_name="static/glossaire_acronymes_pci_ssf.html"),
+        name="glossaire-pci-ssf",
+    ),
     # Django built-in admin (superuser DB management only)
     path("admin/", admin.site.urls),
 ]
